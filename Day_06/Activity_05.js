9ecf345d8a29e29fbceafb1f1b87f3b6f4648591
ShubhamMaurya22 <shubhammaurya8698@gmail.com>
2024-07-21 16:52:53 +0530

    Day 06 done

--- Day_06/Activity_05.js
+++ Day_06/Activity_05.js
@@ -14,3 +14,11 @@ for(let i = 0; i < row; i++){
 console.log(arr)
 
 // Task 02
+const arr2 = [ [ 0, 1, 2 ], [ 3, 4, 5 ], [ 6, 7, 8 ] ]
+for(let i = 0; i < arr2.length; i++){
+    for(let j = 0; j < arr2[i].length; j++){
+        if(arr2[i][j] == 4){
+            console.log(`found the value `, arr2[i][j], 'at index', j)
+        }
+    }
+}
